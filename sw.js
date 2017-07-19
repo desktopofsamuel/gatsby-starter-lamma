5f5f26c700426545c2903e59546f9344adac98a2
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-19 04:34:19 +0400

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-7825b950fc27b16a63bb.js","3d119a222eb16f8993a219baf6ae8f82"],["/commons-b34b373f2547e8b0588f.js","5fb039bba4c3578321382b344a35f50e"],["/index.html","32717263ef05f01f3e8ee022df102317"],["/manifest.json","53da04d3756b99d845311cf0dab7b6fa"],["/offline-plugin-app-shell-fallback/index.html","00a754766c811ef140abeffea945da89"]];
+var precacheConfig = [["/app-f095521fe574856a6744.js","5b1c1dc9c6f4e08d014e6169941850ad"],["/commons-b34b373f2547e8b0588f.js","5fb039bba4c3578321382b344a35f50e"],["/index.html","58d6335a14217f4063838344ddc9521a"],["/manifest.json","53da04d3756b99d845311cf0dab7b6fa"],["/offline-plugin-app-shell-fallback/index.html","ca45b00b4760351936d5428c64f85d74"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
