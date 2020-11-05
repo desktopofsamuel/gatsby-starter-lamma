8f99a958018abc57dbad6e60b1ec38b5012a4ab0
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2020-11-05 23:55:25 +0800

    Container Update

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import mediaQuery from "../../utils/mediaQuery";
 
 const Wrapper = styled.div`
-  max-width: 1280px;
-  padding: 0 24px;
+  max-width: 1024px;
+  width: 100%;
+  padding: 0 1rem;
   margin: 0 auto;
-
-  ${mediaQuery.maxMobile} {
-    display: block;
-    padding: 0 16px;
-  }
 `;
 
 const Container = ({ children }) => {
